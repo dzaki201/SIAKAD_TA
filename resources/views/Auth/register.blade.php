cc9a42d5b0a27f6116790ed5972ea4e94870014b
dzaki201 <muhdzaki206@gmail.com>
2025-04-18 17:09:06 +0700

    feat : halaman register dan login

---
+++ resources/views/Auth/register.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="bg-gray-100 flex justify-center items-center h-screen">
+        <!-- Left: Image -->
+        <div class="w-1/2 h-screen hidden lg:block">
+            <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image"
+                class="object-cover w-full h-full">
+        </div>
+        <!-- Right: Login Form -->
+        <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+            <h1 class="text-2xl font-semibold mb-4">Register</h1>
+            <form action="#" method="POST">
+                <!-- Username Input -->
+                <div class="mb-4">
+                    <label for="username" class="block text-gray-600">Username</label>
+                    <input type="text" id="username" name="username"
+                        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                        autocomplete="off">
+                </div>
+                <!-- Email Input -->
+                <div class="mb-4">
+                    <label for="username" class="block text-gray-600">Email</label>
+                    <input type="email" id="email" name="email"
+                        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                        autocomplete="off">
+                </div>
+                <!-- Password Input -->
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-600">Password</label>
+                    <input type="password" id="password" name="password"
+                        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                        autocomplete="off">
+                </div>
+                <!-- Comfirm Password -->
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-600">Konfirmasi Password</label>
+                    <input type="password" id="password" name="password"
+                        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                        autocomplete="off">
+                </div>
+                <!-- Login Button -->
+                <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Register</button>
+            </form>
+            <!-- Sign up  Link -->
+            <div class="mt-6 text-center">
+                <a class="text-black-500">sudah punya akun?</a>
+                <a href="#" class="text-blue-500 hover:underline">login</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
