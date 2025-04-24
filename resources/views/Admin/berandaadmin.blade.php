b941153f2f0b280f5da224ba9cdaff9b0e28ee85
dzaki201 <muhdzaki206@gmail.com>
2025-04-24 17:49:47 +0700

    feat : navbar sidebar admin

--- resources/views/Admin/berandaadmin.blade.php
+++ resources/views/Admin/berandaadmin.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Admin</title>
 </head>
+
 <body>
-    
+    @include('components.navbar-admin')
+    @include('components.sidebar-admin')
     Ini Halaman Admin
     <a href="{{ route('logout') }}">Logout</a>
 </body>
+
 </html>
