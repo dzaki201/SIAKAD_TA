cd44766c38a6e8ac9c9043bbb7e30d2248ef25d3
dzaki201 <muhdzaki206@gmail.com>
2025-05-25 21:06:01 +0700

    new modal, controller

---
+++ app/Http/Controllers/Ekstrakulikuler/SiswaEkstrakulikuler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Ekstrakulikuler;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiswaEkstrakulikuler extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Nilai/CapaianPembelajaran.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Nilai;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CapaianPembelajaran extends Controller
+{
+    //
+}
