2f2d8cb7afaee2778c6ccdd50d194ba8abc2b736
dzaki201 <muhdzaki206@gmail.com>
2025-06-19 16:39:54 +0700

    revisi search absensi dan ekskul

--- resources/views/Guru/layouts/absensi.blade.php
+++ resources/views/Guru/layouts/absensi.blade.php
@@ -29,8 +29,8 @@
         $tahunAktif = $tahuns->firstWhere('status', 1);
     @endphp
     <div class="overflow-x-auto w-auto rounded-lg border p-4 bg-white dark:bg-gray-800 shadow">
-        @if ($tahun->id == $tahunAktif->id)
-            @if ($siswas->first()->absensi->isEmpty())
+        @if ($siswas->first()->absensi->isEmpty())
+            @if ($tahun->id == $tahunAktif->id)
                 <div class="flex justify-center items-center h-[500px]">
                     <a href="{{ route('guru.absensi.store', ['id' => $tahun->id]) }}">
                         <button class="bg-blue-600 text-white rounded-full p-6 hover:bg-blue-700 focus:outline-none">
@@ -44,65 +44,65 @@
                         {{ $tahun->semester }} - {{ $tahun->tahun }}</span>
                 </div>
             @else
-                <table
-                    class="w-full min-w-[1000px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
-                    <thead class="text-xs text-white text-center uppercase bg-blue-800 dark:bg-gray-700">
-                        <tr>
-                            <th class="w-12 px-4 py-3 text-left border border-gray-300">No</th>
-                            <th class="w-px-4 py-3  border border-gray-300">NIS</th>
-                            <th class="w-px-4 py-3 border border-gray-300">Nama</th>
-                            <th class="w-px-4 py-3 border border-gray-300">Ijin</th>
-                            <th class="w-px-4 py-3 border border-gray-300">Sakit</th>
-                            <th class="w-px-4 py-3 border border-gray-300">Tanpa Keterangan (alpa)</th>
-                            @if ($tahun->id == $tahunAktif->id)
-                                <th class="w-px-4 py-3 border border-gray-300">Aksi</th>
-                            @else
-                            @endif
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($siswas as $siswa)
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="w-12 w-px-4 py-3 border border-gray-300 text-center">{{ $loop->iteration }}</td>
-                                <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nis }}</td>
-                                <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nama }}</td>
-                                @foreach ($siswa->absensi as $absensi)
-                                    <td class="w-px-4 py-3 border border-gray-300 text-center">
-                                        {{ $absensi->sakit ?? '-' }}
-                                    </td>
-                                    <td class="w-px-4 py-3 border border-gray-300 text-center">
-                                        {{ $absensi->ijin ?? '-' }}
-                                    </td>
-                                    <td class="w-px-4 py-3 border border-gray-300 text-center">
-                                        {{ $absensi->alpa ?? '-' }}
-                                    </td>
-                                    @if ($tahun->id == $tahunAktif->id)
-                                        <td class="px-6 py-4 border border-gray-300">
-                                            <button data-modal-target="edit-absensi-modal-{{ $absensi->id }}"
-                                                data-modal-toggle="edit-absensi-modal-{{ $absensi->id }}"
-                                                class="inline-flex items-center bg-yellow-500 text-white p-2 rounded-lg hover:bg-yellow-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" class="size-5 mr-1">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                                </svg>
-                                                Edit
-                                            </button>
-                                        </td>
-                                    @endif
-                                    @include('guru.partials.absensi.modal-edit-absensi')
-                                @endforeach
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="flex justify-center items-center h-[500px]">
+                    <span class="ml-4 text-lg font-semibold text-gray-700 dark:text-white">Tidak ada data absensi di
+                        semester
+                        {{ $tahun->semester }} - {{ $tahun->tahun }} </span>
+                </div>
             @endif
-        @else
-            <div class="flex justify-center items-center h-[500px]">
-                <span class="ml-4 text-lg font-semibold text-gray-700 dark:text-white">Tidak ada data absensi di semester
-                    {{ $tahun->semester }} - {{ $tahun->tahun }} </span>
-            </div>
-        @endif
     </div>
+@else
+    <table class="w-full min-w-[1000px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
+        <thead class="text-xs text-white text-center uppercase bg-blue-800 dark:bg-gray-700">
+            <tr>
+                <th class="w-12 px-4 py-3 text-left border border-gray-300">No</th>
+                <th class="w-px-4 py-3  border border-gray-300">NIS</th>
+                <th class="w-px-4 py-3 border border-gray-300">Nama</th>
+                <th class="w-px-4 py-3 border border-gray-300">Ijin</th>
+                <th class="w-px-4 py-3 border border-gray-300">Sakit</th>
+                <th class="w-px-4 py-3 border border-gray-300">Tanpa Keterangan (alpa)</th>
+                @if ($tahun->id == $tahunAktif->id)
+                    <th class="w-px-4 py-3 border border-gray-300">Aksi</th>
+                @else
+                @endif
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($siswas as $siswa)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="w-12 w-px-4 py-3 border border-gray-300 text-center">{{ $loop->iteration }}</td>
+                    <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nis }}</td>
+                    <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nama }}</td>
+                    @foreach ($siswa->absensi as $absensi)
+                        <td class="w-px-4 py-3 border border-gray-300 text-center">
+                            {{ $absensi->sakit ?? '-' }}
+                        </td>
+                        <td class="w-px-4 py-3 border border-gray-300 text-center">
+                            {{ $absensi->ijin ?? '-' }}
+                        </td>
+                        <td class="w-px-4 py-3 border border-gray-300 text-center">
+                            {{ $absensi->alpa ?? '-' }}
+                        </td>
+                        @if ($tahun->id == $tahunAktif->id)
+                            <td class="px-6 py-4 border border-gray-300">
+                                <button data-modal-target="edit-absensi-modal-{{ $absensi->id }}"
+                                    data-modal-toggle="edit-absensi-modal-{{ $absensi->id }}"
+                                    class="inline-flex items-center bg-yellow-500 text-white p-2 rounded-lg hover:bg-yellow-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 mr-1">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+                                    Edit
+                                </button>
+                            </td>
+                        @endif
+                        @include('guru.partials.absensi.modal-edit-absensi')
+                    @endforeach
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @endif
+
 @endsection
--- resources/views/Guru/layouts/ekskul.blade.php
+++ resources/views/Guru/layouts/ekskul.blade.php
@@ -30,97 +30,101 @@
         $tahunAktif = $tahuns->firstWhere('status', 1);
     @endphp
     <div class="overflow-x-auto w-auto rounded-lg border p-4 bg-white dark:bg-gray-800 shadow">
-        {{-- @if ($tahun->id == $tahunAktif->id) --}}
-
-        <table class="w-full min-w-[1000px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
-            <thead class="text-xs text-white text-center uppercase bg-blue-800 dark:bg-gray-700">
-                <tr>
-                    <th class="w-12 px-4 py-3 text-left border border-gray-300">No</th>
-                    <th class="w-px-4 py-3  border border-gray-300">NIS</th>
-                    <th class="w-px-4 py-3 border border-gray-300 ">Nama</th>
-                    <th class="w-px-4 py-3 border border-gray-300 ">Ekskul</th>
-                    <th class="w-px-4 py-3 border border-gray-300 ">Keterangan</th>
-                    @if ($tahun->id == $tahunAktif->id)
-                        <th class="w-px-4 py-3 border border-gray-300 " colspan="2">Aksi</th>
-                    @endif
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($siswas as $siswa)
-                    @php $rowspan = $siswa->siswaekskul->count() ?: 1; @endphp
-                    @foreach ($siswa->siswaekskul as $ekskulIndex => $ekskul)
-                        <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            @if ($ekskulIndex == 0)
-                                <td class="w-px-4 py-3  border border-gray-300 text-center" rowspan="{{ $rowspan }}">
-                                    {{ $no }}</td>
-                                <td class="w-px-4 py-3  border border-gray-300 text-center" rowspan="{{ $rowspan }}">
-                                    {{ $siswa->nis }}</td>
-                                <td class="w-px-4 py-3  border border-gray-300 text-center" rowspan="{{ $rowspan }}">
-                                    {{ $siswa->nama }}</td>
-                            @endif
-                            <td class="w-px-4 py-3  border border-gray-300 text-center">
-                                {{ $ekskul->ekskul->nama ?? '-' }}
-                            </td>
-                            <td class="w-px-4 py-3  border border-gray-300 text-center">
-                                {{ $ekskul->keterangan ?? '-' }}
-                            </td>
-                            @if ($tahun->id == $tahunAktif->id)
+        @if ($siswas->every(fn($s) => $s->siswaekskul->isEmpty()))
+            <div class="flex justify-center items-center h-[500px]">
+                <span class="ml-4 text-lg font-semibold text-gray-700 dark:text-white">Tidak ada data absensi di
+                    semester
+                    {{ $tahun->semester }} - {{ $tahun->tahun }} </span>
+            </div>
+        @else
+            <table
+                class="w-full min-w-[1000px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
+                <thead class="text-xs text-white text-center uppercase bg-blue-800 dark:bg-gray-700">
+                    <tr>
+                        <th class="w-12 px-4 py-3 text-left border border-gray-300">No</th>
+                        <th class="w-px-4 py-3  border border-gray-300">NIS</th>
+                        <th class="w-px-4 py-3 border border-gray-300 ">Nama</th>
+                        <th class="w-px-4 py-3 border border-gray-300 ">Ekskul</th>
+                        <th class="w-px-4 py-3 border border-gray-300 ">Keterangan</th>
+                        @if ($tahun->id == $tahunAktif->id)
+                            <th class="w-px-4 py-3 border border-gray-300 " colspan="2">Aksi</th>
+                        @endif
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($siswas as $siswa)
+                        @php $rowspan = $siswa->siswaekskul->count() ?: 1; @endphp
+                        @foreach ($siswa->siswaekskul as $ekskulIndex => $ekskul)
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                @if ($ekskulIndex == 0)
+                                    <td class="w-px-4 py-3  border border-gray-300 text-center"
+                                        rowspan="{{ $rowspan }}">
+                                        {{ $no }}</td>
+                                    <td class="w-px-4 py-3  border border-gray-300 text-center"
+                                        rowspan="{{ $rowspan }}">
+                                        {{ $siswa->nis }}</td>
+                                    <td class="w-px-4 py-3  border border-gray-300 text-center"
+                                        rowspan="{{ $rowspan }}">
+                                        {{ $siswa->nama }}</td>
+                                @endif
                                 <td class="w-px-4 py-3  border border-gray-300 text-center">
-                                    <button data-modal-target="edit-ekskul-modal-{{ $ekskul->id }}"
-                                        data-modal-toggle="edit-ekskul-modal-{{ $ekskul->id }}"
-                                        class="inline-flex items-center bg-yellow-500 text-white p-2 rounded-lg hover:bg-yellow-600">
-                                        Edit Nilai
-                                    </button>
-                                    <button data-modal-target="hapus-ekskul-modal-{{ $ekskul->id }}"
-                                        data-modal-toggle="hapus-ekskul-modal-{{ $ekskul->id }}"
-                                        class="inline-flex items-center bg-red-500 text-white p-2 rounded-lg hover:bg-red-600">
-                                        Hapus Ekskul
-                                    </button>
+                                    {{ $ekskul->ekskul->nama ?? '-' }}
                                 </td>
-                            @endif
-                            @if ($ekskulIndex == 0 && $tahun->id == $tahunAktif->id)
-                                <td class="w-px-4 py-3  border border-gray-300 text-center" rowspan="{{ $rowspan }}">
+                                <td class="w-px-4 py-3  border border-gray-300 text-center">
+                                    {{ $ekskul->keterangan ?? '-' }}
+                                </td>
+                                @if ($tahun->id == $tahunAktif->id)
+                                    <td class="w-px-4 py-3  border border-gray-300 text-center">
+                                        <button data-modal-target="edit-ekskul-modal-{{ $ekskul->id }}"
+                                            data-modal-toggle="edit-ekskul-modal-{{ $ekskul->id }}"
+                                            class="inline-flex items-center bg-yellow-500 text-white p-2 rounded-lg hover:bg-yellow-600">
+                                            Edit Nilai
+                                        </button>
+                                        <button data-modal-target="hapus-ekskul-modal-{{ $ekskul->id }}"
+                                            data-modal-toggle="hapus-ekskul-modal-{{ $ekskul->id }}"
+                                            class="inline-flex items-center bg-red-500 text-white p-2 rounded-lg hover:bg-red-600">
+                                            Hapus Ekskul
+                                        </button>
+                                    </td>
+                                @endif
+                                @if ($ekskulIndex == 0 && $tahun->id == $tahunAktif->id)
+                                    <td class="w-px-4 py-3  border border-gray-300 text-center"
+                                        rowspan="{{ $rowspan }}">
+                                        <button data-modal-target="tambah-ekskul-modal-{{ $siswa->id }}"
+                                            data-modal-toggle="tambah-ekskul-modal-{{ $siswa->id }}"
+                                            class="inline-flex items-center bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700">
+                                            Tambah Ekskul
+                                        </button>
+                                    </td>
+                                @endif
+                            </tr>
+                            @include('guru.partials.eksktrakulikuler.modal-edit-ekskul')
+                            @include('guru.partials.eksktrakulikuler.modal-hapus-ekskul')
+                        @endforeach
+                        @if ($siswa->siswaekskul->isEmpty())
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $no }}</td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nis }}</td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nama }}</td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">-</td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">-</td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center"></td>
+                                <td class="w-px-4 py-3 border border-gray-300 text-center">
                                     <button data-modal-target="tambah-ekskul-modal-{{ $siswa->id }}"
                                         data-modal-toggle="tambah-ekskul-modal-{{ $siswa->id }}"
-                                        class="inline-flex items-center bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700">
+                                        class="inline-flex items-center bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600">
                                         Tambah Ekskul
                                     </button>
                                 </td>
-                            @endif
-                        </tr>
-                        @include('guru.partials.eksktrakulikuler.modal-edit-ekskul')
-                        @include('guru.partials.eksktrakulikuler.modal-hapus-ekskul')
+                            </tr>
+                        @endif
+                        @php $no++; @endphp
+                        @include('guru.partials.eksktrakulikuler.modal-tambah-ekskul')
                     @endforeach
-                    @if ($siswa->siswaekskul->isEmpty())
-                        <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $no }}</td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nis }}</td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">{{ $siswa->nama }}</td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">-</td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">-</td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center"></td>
-                            <td class="w-px-4 py-3 border border-gray-300 text-center">
-                                <button data-modal-target="tambah-ekskul-modal-{{ $siswa->id }}"
-                                    data-modal-toggle="tambah-ekskul-modal-{{ $siswa->id }}"
-                                    class="inline-flex items-center bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600">
-                                    Tambah Ekskul
-                                </button>
-                            </td>
-                        </tr>
-                    @endif
-                    @php $no++; @endphp
-                    @include('guru.partials.eksktrakulikuler.modal-tambah-ekskul')
-                @endforeach
-            </tbody>
-        </table>
-        {{-- @else
-                <div class="flex justify-center items-center h-[500px]">
-                    <span class="ml-4 text-lg font-semibold text-gray-700 dark:text-white">Tidak ada data absensi di
-                        semester
-                        {{ $tahun->semester }} - {{ $tahun->tahun }} </span>
-                </div>
-            @endif --}}
+                </tbody>
+            </table>
+        @endif
     </div>
 @endsection
